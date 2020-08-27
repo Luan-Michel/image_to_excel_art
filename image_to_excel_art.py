f17e3d8689a5b5eb4d2bd87ea65ae28a205abdec
Luan Michel <luanmichel96@gmail.com>
2020-08-26 21:14:27 -0300

    Linux adjusts and keep proportion

--- image_to_excel_art.py
+++ image_to_excel_art.py
@@ -5,30 +5,41 @@ from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 from tkinter.filedialog import asksaveasfile
 
-def coluna_excel(n):
-    divisao = int(n / 26)
-    resto = n % 26
-    if(divisao > 0):
-        return coluna_excel(divisao-1)+chr(resto+65)
-    if(divisao == 0):
-        return chr(resto+65)
+def calc_col(n):
+    div = int(n / 26)
+    mod = n % 26
+    if(div > 0):
+        return calc_col(div-1)+chr(mod+65)
+    if(div == 0):
+        return chr(mod+65)
 
-Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+def keep_proportion(large_side, width, height):
+    if (width > height):
+        return (int((large_side / width) * height), large_side)
+    else:
+        return (large_side, int((large_side / height) * width))
 
-save_name = asksaveasfile(mode='a', filetype = [('Excel', '*.xlsx')], defaultextension=[('Excel', '*.xlsx')])
+
+LARGE_SIDE_RESOLUTION = 200
+
+Tk().withdraw()                 # we don't want a full GUI, so keep the root window from appearing
+# show an "Open" dialog box and return the path to the selected file
+filename = askopenfilename(
+    filetypes=[('PNG', '.png')])
+
+save_name = asksaveasfile(mode='w', filetypes = [('Excel', '.xlsx')], defaultextension='.xlsx')
 
 img = Image.open(filename)
-img = img.resize((200, 200), Image.ANTIALIAS)
-rgb_im = img.convert('RGB')
-print(img.size)
 width, height = img.size
-arquivo_excel = Workbook()
-planilha1 = arquivo_excel.active
-for i in range(1, width):
-    planilha1.column_dimensions[coluna_excel(i-1)].width = 3
-    for j in range(1, height):
+n_width, n_height = keep_proportion(LARGE_SIDE_RESOLUTION, width, height)
+img = img.resize((n_width, n_height), Image.ANTIALIAS)
+rgb_im = img.convert('RGB')
+excel_file = Workbook()
+sheet = excel_file.active
+for i in range(1, n_width):
+    sheet.column_dimensions[calc_col(i-1)].width = 3
+    for j in range(1, n_height):
         string = '%02x%02x%02x' % rgb_im.getpixel((i, j))
-        planilha1.cell(row=j, column=i).fill = PatternFill(
+        sheet.cell(row=j, column=i).fill = PatternFill(
             start_color=string, end_color=string, fill_type="solid")
-arquivo_excel.save(save_name.name)
+excel_file.save(save_name.name)
